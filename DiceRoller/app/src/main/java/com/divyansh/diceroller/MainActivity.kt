b64dd98fae514c5d5683cc9d8d23a9325e6a532d
justdvnsh <justdvnsh2208@gmail.com>
2020-05-09 00:30:08 +0530

    completed the first dice rolling app

--- DiceRoller/app/src/main/java/com/divyansh/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/divyansh/diceroller/MainActivity.kt
@@ -2,11 +2,36 @@ package com.divyansh.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollBtn: Button = findViewById(R.id.roll_button)
+        rollBtn.setOnClickListener {
+            rollDice()
+        }
     }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+
 }
