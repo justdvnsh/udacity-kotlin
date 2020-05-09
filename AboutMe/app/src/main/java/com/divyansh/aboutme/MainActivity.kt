2a427bab7f21e605c22a19e58161aa4fe4bc7dd7
justdvnsh <justdvnsh2208@gmail.com>
2020-05-09 15:15:06 +0530

    added data binding to the project

--- AboutMe/app/src/main/java/com/divyansh/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/divyansh/aboutme/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.divyansh.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.divyansh.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneBtn.setOnClickListener {
+            addNickname()
+        }
     }
+
+    private fun addNickname() {
+        binding.apply {
+            val nickNameString = nickname.text
+            nicknameText.setText(nickNameString)
+            invalidateAll()
+            nickname.visibility = GONE
+            doneBtn.visibility = GONE
+            nicknameText.visibility = VISIBLE
+        }
+
+        // hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.doneBtn.windowToken, 0)
+    }
+
+
 }
